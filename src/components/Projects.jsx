1b133eee425c2936eec53a1f1fff703b17706f54
Vaibhaw-P <strategicpandey@gmail.com>
2025-12-10 16:53:53 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,17 +5,17 @@ const Projects = () => {
   const projects = [
     {
       title: 'Project 1',
-      description: 'A brief description of the project.',
-      technologies: ['HTML', 'CSS', 'JavaScript'],
-      github: 'https://github.com/Vaibhaw-P',
-      demo: 'https://example.com',
+      description: 'chatssssss.netlify.app is a lightweight, fast, and responsive web-based chat interface designed using modern web technologies to provide a smooth, minimal, and user-friendly messaging experience',
+      technologies: ['HTML', 'CSS', 'JavaScript', 'Netlify', 'Vercel',],
+      github: 'https://github.com/Vaibhaw-P/Chat_App-Frontend',
+      demo: 'https://Chatssssss.netlify.app',
     },
     {
       title: 'Project 2',
-      description: 'A brief description of the project.',
-      technologies: ['React', 'Figma'],
-      github: 'https://github.com/Vaibhaw-P',
-      demo: 'https://example.com',
+      description: 'pokedexii.netlify.app is a simple and responsive Pokédex web app that fetches real-time Pokémon data using JavaScript and displays it through a clean, interactive HTML/CSS interface.',
+      technologies: ['HTML', 'CSS', 'JavaScript',],
+      github: 'https://github.com/Vaibhaw-P/Pokedex',
+      demo: 'https://Pokedexii.netlify.app',
     },
   ];
 
